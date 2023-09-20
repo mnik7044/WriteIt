03aefe31ade374173cae293209aaf8460d877e8a
mnik7044 <mnikwork7802@gmail.com>
2023-09-21 04:12:00 +0530

    Home Page DOne

---
+++ client/src/App.js
@@ -0,0 +1,71 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <header>
+        <a href="" className="logo">My Blogs</a>
+        <nav>
+        <a href="" className="">Login</a>
+        <a href="" className="">Register</a>
+        </nav>
+      </header>
+      <div className="post"> 
+      <div className ="image">
+       <img src="https://cdn.hashnode.com/res/hashnode/image/upload/v1692151081466/d944246e-3c03-4ba8-87d6-353bc6586a6a.png?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp" />
+      </div>
+      <div className="text"> 
+       <h2>Transitioning from JavaScript to TypeScript</h2>
+       <p className= "info">
+        <a className ="author">Nikhil Mishra</a>
+        <time>21-09-2023 04:00</time>
+       </p>
+       <p className = "summary">Transitioning from JavaScript to TypeScript: A Guide for JavaScript DevelopersTransitioning from JavaScript to TypeScript: A Guide for JavaScript Developers</p>
+      </div>
+      </div>
+      <div className="post"> 
+      <div className ="image">
+       <img src="https://cdn.hashnode.com/res/hashnode/image/upload/v1692151081466/d944246e-3c03-4ba8-87d6-353bc6586a6a.png?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp" />
+      </div>
+      <div className="text"> 
+       <h2>Transitioning from JavaScript to TypeScript</h2>
+       <p className= "info">
+        <a className ="author">Nikhil Mishra</a>
+        <time>21-09-2023 04:00</time>
+       </p>
+       <p className = "summary">Transitioning from JavaScript to TypeScript: A Guide for JavaScript DevelopersTransitioning from JavaScript to TypeScript: A Guide for JavaScript Developers</p>
+      </div>
+      </div>
+      <div className="post"> 
+      <div className ="image">
+       <img src="https://cdn.hashnode.com/res/hashnode/image/upload/v1692151081466/d944246e-3c03-4ba8-87d6-353bc6586a6a.png?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp" />
+      </div>
+      <div className="text"> 
+       <h2>Transitioning from JavaScript to TypeScript</h2>
+       <p className= "info">
+        <a className ="author">Nikhil Mishra</a>
+        <time>21-09-2023 04:00</time>
+       </p>
+       <p className = "summary">Transitioning from JavaScript to TypeScript: A Guide for JavaScript DevelopersTransitioning from JavaScript to TypeScript: A Guide for JavaScript Developers</p>
+      </div>
+      </div>
+      <div className="post"> 
+      <div className ="image">
+       <img src="https://cdn.hashnode.com/res/hashnode/image/upload/v1692151081466/d944246e-3c03-4ba8-87d6-353bc6586a6a.png?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp" />
+      </div>
+      <div className="text"> 
+       <h2>Transitioning from JavaScript to TypeScript</h2>
+       <p className= "info">
+        <a className ="author">Nikhil Mishra</a>
+        <time>21-09-2023 04:00</time>
+       </p>
+       <p className = "summary">Transitioning from JavaScript to TypeScript: A Guide for JavaScript DevelopersTransitioning from JavaScript to TypeScript: A Guide for JavaScript Developers</p>
+      </div>
+      </div>
+      
+    </main>
+  );
+}
+
+export default App;
